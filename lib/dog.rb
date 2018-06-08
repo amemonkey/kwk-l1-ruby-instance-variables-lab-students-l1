22a0646512e08cb12d6548ae08c5248e1131258c
amemonkey <ameliafunaki789@gmail.com>
2018-06-08 20:44:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,22 @@
 # dog.rb
 
 
-def bro
-  bro_greeting = "Sup, Bro?😂"
-  bro_greeting.upcase 
-end 
+# def bro
+#   bro_greeting = "Sup, Bro?😂"
+#   bro_greeting.upcase 
+# end 
 
-puts bro
+# puts bro
+
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
+end
+
+lassie = Dog.new 
+lassie.name = "Lassie"
